9a06a577cbe1f5b755b8edbbbece965edcf3c03c
Mindy DeWaal <kawiititan@gmail.com>
2023-09-28 18:21:41 -0700

    Able to train a model.

--- classifier.py
+++ classifier.py
@@ -2,8 +2,8 @@
 
 # add-on modules
 
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras import metrics
+from keras.callbacks import TensorBoard
+from keras import metrics
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -11,13 +11,91 @@ from sklearn.metrics import ConfusionMatrixDisplay
 from feature_extraction import get_features
 
 def train(model, features, labels, epochs_n=20):
-    # Pass in network model, training feature matrix and labels
-    # train for specified number of epochs_n (use a low number
-    # while developing your code
-    pass
+    """
+    Pass in network model, training feature matrix and labels
+    train for specified number of epochs_n (use a low number
+    while developing your code
 
-def test(model, corpus, test_utterances, adv_ms, len_ms):
-    # execute all, print error rate and return confusion matrix
-    pass
+    Reference Kera Model Training APIs
+    https://keras.io/api/models/model_training_apis/
+    """
+    # Split your training data (see scipy) with a 90/10 split to use 90% of it for 
+    # training and 10% for validation. The validation data can be provided to keras’s fit 
+    # function and can show you how your model is performing during training. Do not expect 
+    # wonderful accuracy at this stage.
+    model.compile()
+    model.fit(x=features, 
+              y=labels, 
+              validation_split=.1, # data is selected from the last samples in the x and y data provided
+              shuffly=True,
+              epochs=epochs_n)
+    
+    # Run metrics?
+    #TODO: View Accuracy
+    print(f"Accurance of Model")
+
+    return model
+
+def split_feature_label(speakers, corpus, encoder, adv_ms, len_ms):
+    samples = []
+    labels  = []
+    for i in speakers:
+        for file in corpus[i]:
+            features, new_labels = get_features(file, adv_ms, len_ms, i)
+            new_labels = encoder.transform(new_labels.reshape(-1, 1)).A
+
+            if(samples is not None):
+                samples.append(features)
+                labels.append(new_labels)
+
+    samples = np.concatenate(samples)
+    labels  = np.concatenate(labels)
+
+    return samples, labels
+
+
+def test(model, corpus, test_utterances, adv_ms, len_ms, one_hot_encoder):
+    """
+    Execute all, print error rate and return confusion matrix
+    """ 
+    x, y = split_feature_label(test_utterances, corpus, one_hot_encoder, adv_ms, len_ms):
+    one_hot_encoded_label = one_hot_encoder(y[0])
+    target_label = test_utterances
+
+    # Returns an array with containing 1 entry for each utterance given
+    # Each element in the array is the speaker the model thought the utterance belonged to
+    predictions = model.predict(x) 
+
+    # Assume that frames are independent and take the product of the probabilities. 
+    # Do so in the log domain (multiplication becomes addition) or your computation will underflow.
+   
+    # Summation across columns where each column holds the
+    # probability of a speaker's identification  
+    probability_per_feature = np.sum(predictions, axis=0)
+    
+    # argmax retuns the index with the highest prediction score
+    index_predicted = np.argmax(probability_per_feature)
+    # Find the index that occurs the most
+    target_predicted = corpus.category_to_speaker(index_predicted)
+    
+    print(f"Actual Speaker: {target_label}")
+    print(f"Predicted Speaker: {target_predicted}")
+
+    # Figure out how many of those labels were the target speaker
+    error_rate = correct_predictions / len(y)
+
+    # Add prediction to the confusion matrix
+    n=1
+    np.zeros((n,n))
+
+    # Confusion Matrix for each speaker classified
+    conf_matrix = ConfusionMatrixDisplay(display_labels=test_utterances)
+
+    disp = ConfusionMatrixDisplay(confusion_matrix=[predictions, labels],
+                                display_labels=test_utterances[0])
+
+    print(f"Error Rate: {error_rate}")
+
+    return conf_matrix
 
 
--- driver.py
+++ driver.py
@@ -1,11 +1,15 @@
-from argparse import ArgumentParser
+"""
+I promise that the attached assignment is my own work. 
+I recognize that should this not be the case,
+I will be subject to penalties as outlined in the course syllabus.
+Mindy Flores
+"""
 
+from argparse import ArgumentParser
 import numpy as np
-from tensorflow.keras.utils import to_categorical
+from keras.utils import to_categorical
 import matplotlib.pyplot as plt
 import matplotlib
-# add-on packages
-
 
 # project imports
 from library.corpus import King
@@ -14,32 +18,68 @@ from library.timer import Timer
 from feature_extraction import get_features
 from architecture import get_model
 from classifier import train, test
-
-
-
-
-
+from sklearn.preprocessing import OneHotEncoder
 
 def main(args):
     # Specify how matplotlib renders (backend library)
-    # MacoOS:  'MacOSX' - native MacOS
-    # TkAgg - Tcl/Tk backend
-    # Qt5Agg:  Qt library, must be installed, e.g. module pyside2
-    matplotlib.use("TkAgg")
+    matplotlib.use("QtAgg")
     plt.ion()
 
-    king = King(args.king, group="Nutley")  # Create instance of King corpus
+    adv_ms = args.adv_ms
+    len_ms = args.len_ms
 
-    # Conduct the experiment
-    # Need to load in appropriate features and labels
+    # Expect speakers 27 to 60 to be availible
+    # Total: 25 speakers
+    king = King(args.king, group="Nutley")
+    
+    speakers = list(king.get_speakers()) # We can use catagory_2_speaker to translate index to speaker ID
+    encorder = create_one_hot(speakers)
+    training_speakers = speakers[:5]
+    testing_speakers = speakers[5:]
+
+    # Training data
+    train_samples = None
+    train_labels  = None
+    for i in training_speakers:
+        for file in king[i]:
+            features, labels = get_features(file, adv_ms, len_ms, i)
+            labels = encorder.transform(labels.reshape(-1, 1)).A
+
+            if(train_samples is not None):
+                train_samples = np.concatenate([train_samples, features])
+                train_labels  = np.concatenate([train_labels, labels])
+            else:
+                train_samples = features
+                train_labels = labels
+
+    # Initialize model
+    feature_width = len(train_samples[0])
+    total_speakers = len(speakers) # Gives us the total categories
+    base_model = get_model('l2', feature_width, 3, 20, .01, total_speakers)
+
     # train the model against the first 5 utterances of each speaker
-    # test against the second 5 of each speaker
-    # and compute error rate.  Use train and test functions
-    # and add any others you find appropriate
+    train(base_model, train_samples, train_labels, epochs_n=3)
 
+    # # Load in all features related to a single speaker
+    confusion_matrix = test(base_model, king, testing_speakers[0], adv_ms, len_ms, encorder)
+    confusion_matrix.plot()
+    plt.show()
 
+def format_one_hot(labels):
+    categories = []
+    for speaker in labels:
+        categories.append([speaker])
+    return categories
 
+def create_one_hot(speaker_list):
+    # Create the list of categories in the
+    # format expected of scikit-learn 
+    categories = format_one_hot(speaker_list)
 
+    # Generate the one-hot encorder
+    one_hot = OneHotEncoder()
+    one_hot.fit(categories)
+    return one_hot
 
 
 if __name__ == "__main__":
